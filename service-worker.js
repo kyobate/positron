117675638fba97aa78dc99b5e471b7a4b9100615
kyobate <reservoir_chronicle@icloud.com>
2025-03-11 04:14:33 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -7,18 +7,15 @@ const urlsToCache = [
     "/positron/icon.png"
 ];
 
-self.addEventListener("install", event => {
-    event.waitUntil(
-        caches.open(CACHE_NAME).then(cache => {
-            return cache.addAll(urlsToCache);
-        })
-    );
+self.addEventListener("install", (event) => {
+  console.log("Service Worker installing.");
+  self.skipWaiting();
 });
 
-self.addEventListener("fetch", event => {
-    event.respondWith(
-        caches.match(event.request).then(response => {
-            return response || fetch(event.request);
-        })
-    );
+self.addEventListener("activate", (event) => {
+  console.log("Service Worker activated.");
+});
+
+self.addEventListener("fetch", (event) => {
+  console.log("Fetching:", event.request.url);
 });
