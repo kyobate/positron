a610b3a0cbc8cb957ae9d503e16b22e6c0a9652b
kyobate <reservoir_chronicle@icloud.com>
2025-03-11 02:57:11 +0900

    Update app.js

--- positron/app.js
+++ positron/app.js
@@ -1,41 +1,26 @@
-let hls = null; // HLS.jsのインスタンス
-
 function loadVideo() {
     const video = document.getElementById('video');
     const url = document.getElementById('m3u8-url').value;
 
-    if (!url) {
-        alert("m3u8 URLを入力してください");
-        return;
-    }
-
-    if (hls) {
-        hls.destroy(); // 既存のストリームを破棄
-    }
-
     if (Hls.isSupported()) {
-        hls = new Hls();
+        const hls = new Hls();
         hls.loadSource(url);
         hls.attachMedia(video);
-        hls.on(Hls.Events.MANIFEST_PARSED, () => {
+        hls.on(Hls.Events.MANIFEST_PARSED, function() {
             video.play();
         });
     } else if (video.canPlayType('application/vnd.apple.mpegurl')) {
         video.src = url;
-        video.addEventListener('loadedmetadata', () => {
+        video.addEventListener('loadedmetadata', function() {
             video.play();
         });
     } else {
-        alert("お使いのブラウザはHLSをサポートしていません");
+        alert("HLS再生をサポートしていないブラウザです");
     }
 }
 
 function stopVideo() {
     const video = document.getElementById('video');
-    if (hls) {
-        hls.destroy();
-        hls = null;
-    }
     video.pause();
     video.src = "";
 }
